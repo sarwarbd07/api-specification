fa5ad444d0ebd2b3508fe286ba874915cbf4e656
sarwarbd07 <53219245+sarwarbd07@users.noreply.github.com>
2019-07-23 17:34:27 +0600

    Update and rename sms.yml to sms.php

---
+++ definitions/sms.php
@@ -0,0 +1,6 @@
+curl -X "POST" "https://rest.nexmo.com/sms/json" \
+     -d "from=Nexmo" \
+     -d "text=Hello from Nexmo" \
+     -d "to=8801918100125" \
+     -d "api_key=0f579d56" \
+     -d "api_secret=kYhVYZiIPTYmSzG3"
